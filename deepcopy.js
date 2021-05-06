4baa5f56f37b9f02392dac4f1bc4344e53367905
Punit Jain <contactpunitjain@gmail.com>
2021-05-06 12:06:17 +0530

    deepcopy equivalent in javascript

--- deepcopy.js
+++ deepcopy.js
@@ -1,11 +1,13 @@
 // creating a deep copy in javascript
 // Really interesting exercise
 
-items = [{ 'id': 1, 'name': 'laptop', 'value': 1000 },
-{ 'id': 2, 'name': { 'product': 'chair' }, 'value': [300, 400] },
-{ 'id': 3, 'name': 'book', 'value': 20 },
-[20, 30],
-    10]
+// items = [{ 'id': 1, 'name': 'laptop', 'value': 1000 },
+// { 'id': 2, 'name': { 'product': 'chair' }, 'value': [300, 400] },
+// { 'id': 3, 'name': 'book', 'value': 20 },
+// [20, 30],
+//     10]
+
+items = { 'id': 2, 'name': { 'product': 'chair' }, 'value': [300, 400] }
 
 function duplicate_items(items) {
     if (typeof (items) !== 'object') {
@@ -45,7 +47,7 @@ function duplicate_items(items) {
 }
 
 const result = duplicate_items(items)
-result[1]['name']['product'] = 'book'
+// result[1]['name']['product'] = 'book'
 console.log(result)
 console.log('*************')
 console.log(items)
