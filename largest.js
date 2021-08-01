e432e80160e2e1598644abaeee116536fde1e74f
Punit Jain <contactpunitjain@gmail.com>
2021-08-01 15:12:09 +0530

    find largest n values

---
+++ largest.js
@@ -0,0 +1,43 @@
+numbers = [0, -1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6]
+
+dates = [new Date(2018, 1, 23, 0, 0),
+new Date(2017, 12, 19, 0, 0),
+new Date(2017, 10, 15, 0, 0),
+new Date(2019, 2, 27, 0, 0),
+new Date(2017, 3, 29, 0, 0),
+new Date(2018, 8, 11, 0, 0),
+new Date(2018, 5, 3, 0, 0),
+new Date(2018, 12, 19, 0, 0),
+new Date(2018, 11, 19, 0, 0),
+new Date(2017, 7, 7, 0, 0)]
+
+earnings_mln = [
+    { 'name': 'Kevin Durant', 'earnings': 60.6 },
+    { 'name': 'Adele', 'earnings': 69 },
+    { 'name': 'Lionel Messi', 'earnings': 80 },
+    { 'name': 'J.K. Rowling', 'earnings': 95 },
+    { 'name': 'Elton John', 'earnings': 60 },
+    { 'name': 'Chris Rock', 'earnings': 57 },
+    { 'name': 'Justin Bieber', 'earnings': 83.5 },
+    { 'name': 'Cristiano Ronaldo', 'earnings': 93 },
+    { 'name': 'Beyoncé Knowles', 'earnings': 105 },
+    { 'name': 'Jackie Chan', 'earnings': 49 },
+]
+
+function findLargest(data, n = 3) {
+    if (typeof data[0] === 'number') {
+        return data.sort().reverse().slice(0, n)
+    }
+    else if (Object.prototype.toString.call(data[0]) === "[object Date]") {
+        return data.sort((a, b) => {
+            return a - b
+        }).reverse().slice(0, n)
+    }
+    else if (typeof data[0] === 'object' && !Array.isArray(data[0])) {
+        return data.sort((a, b) => {
+            return a.earnings - b.earnings
+        }).reverse().slice(0, n)
+    }
+}
+
+console.log(findLargest(earnings_mln))
